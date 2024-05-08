47a8f10444805d2bb942a39964a5ef8266ed8c6b
April Hussey <april.hussey@outlook.com>
2024-05-08 17:48:12 +0100

    Added mouse pan functionality

--- Games Dissertation/Assets/Scripts/Pan.cs
+++ Games Dissertation/Assets/Scripts/Pan.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Pan : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private GameObject mainCamera;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	private float panSpeed = 5f;
+
+	public void OnMousePan(InputAction.CallbackContext context)
+	{
+		Vector2 delta = context.ReadValue<Vector2>();
+
+		Vector3 movement = new Vector3(-delta.x, 0, -delta.y) * panSpeed * Time.deltaTime;  // Pan affects the x and z axis
+		this.transform.Translate(movement, Space.World);
+	}
+
+	public void OnTouchPan(InputAction.CallbackContext context)
+	{
+
+	}
 }
