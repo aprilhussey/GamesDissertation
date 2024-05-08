ec6bf2954eab2ebc7a33a5ebdd0de6e607c23536
April Hussey <april.hussey@outlook.com>
2024-05-08 23:13:18 +0100

    Replaced board tiles with new prefabs
    New board tiles have the board tile script on them

---
+++ Games Dissertation/Assets/Scripts/BoardTile.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardTile : MonoBehaviour
+{
+    private enum BoardTileColor
+    {
+        black,
+        white
+    }
+
+    [SerializeField]
+    private BoardTileColor boardTileColor;
+}
