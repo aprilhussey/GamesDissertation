178a51875aa42a86e5f80bb426a4d7da98baf288
April Hussey <54184656+aprilhussey@users.noreply.github.com>
2024-05-09 21:58:52 +0100

    Added Board script to board game object

--- Games Dissertation/Assets/Scripts/Board.cs
+++ Games Dissertation/Assets/Scripts/Board.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-	private List<List<Tile>> board;
+	private List<List<Tile>> board = new List<List<Tile>>();
 
 	void Awake()
 	{
+		foreach (Transform child in this.transform)
+		{
+			Row row = child.GetComponent<Row>();
 
+			if (row != null)
+			{
+				board.Add(row.GetRow);
+			}
+		}
+	}
+
+	public List<List<Tile>> GetBoard
+	{
+		get { return board; }
 	}
 }
