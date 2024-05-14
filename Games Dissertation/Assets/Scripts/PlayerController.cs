d92204779be9ed2e87bee7d9cb89a81ec40644ce
April Hussey <april.hussey@outlook.com>
2024-05-14 18:27:41 +0100

    Player now spawns in Game scene

--- Games Dissertation/Assets/Scripts/PlayerController.cs
+++ Games Dissertation/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+	// Photon
+	[HideInInspector]
+	public PhotonView photonView;
+
 	// Input actions
 	private PlayerInput playerInput;
 	private InputActionAsset inputActionAsset;
@@ -12,7 +17,6 @@ public class PlayerController : MonoBehaviour
 
 	private Vector2 screenPosition;
 
-	private Camera mainCamera;
 	private Board board;
 
 	// Moving checkers
@@ -23,6 +27,11 @@ public class PlayerController : MonoBehaviour
 
 	[Header("Camera Settings")]
 	[SerializeField]
+	private GameObject playerCameraController;
+	[SerializeField]
+	private Camera playerCamera;
+
+	[SerializeField]
 	private float panSpeed = 5f;
 
 	[SerializeField]
@@ -32,12 +41,20 @@ public class PlayerController : MonoBehaviour
 	private float yAngle;
 	private float zAngle;
 
+	[Header("Audio Listener")]
+	[SerializeField]
+	private AudioListener playerAudioListener;
+
 	// Player checker color
 	[SerializeField]
 	private Checker.CheckerColor playerCheckerColor;
 
 	void Awake()
     {
+		// Photon
+		photonView = this.GetComponent<PhotonView>();
+
+		// Input actions
 		playerInput = this.GetComponent<PlayerInput>();
 
 		inputActionAsset = playerInput.actions;
@@ -45,15 +62,31 @@ public class PlayerController : MonoBehaviour
 
 		inputActionAsset.Enable();
 
-		mainCamera = Camera.main;
 		board = FindObjectOfType<Board>();
 
-		// Zoom
-		xAngle = mainCamera.transform.rotation.eulerAngles.x;
-		yAngle = mainCamera.transform.rotation.eulerAngles.y;
-		zAngle = mainCamera.transform.rotation.eulerAngles.z;
+		// Camera settings
+		xAngle = playerCamera.transform.rotation.eulerAngles.x;
+		yAngle = playerCamera.transform.rotation.eulerAngles.y;
+		zAngle = playerCamera.transform.rotation.eulerAngles.z;
 
-		mainCamera.transform.rotation = Quaternion.Euler(xAngle, yAngle, zAngle);
+		playerCamera.transform.rotation = Quaternion.Euler(xAngle, yAngle, zAngle);
+	}
+
+	void Start()
+	{
+		if (photonView != null)
+		{
+			if (photonView.IsMine)
+			{
+				playerCameraController.SetActive(true);
+				playerAudioListener.enabled = true;
+			}
+			else
+			{
+				playerCameraController.SetActive(false);
+				playerAudioListener.enabled = false;
+			}
+		}
 	}
 
     void OnEnable()
@@ -61,6 +94,14 @@ public class PlayerController : MonoBehaviour
 		playerActionMap["Press"].performed += OnPress;
 		playerActionMap["Pan"].performed += OnPan;
 		playerActionMap["Zoom"].performed += OnZoom;
+	}
+
+	void Update()
+	{
+		if (photonView != null)
+		{
+			if (!photonView.IsMine && PhotonNetwork.IsConnected) return;
+		}
 	}
 
     void OnDisable()
@@ -72,10 +113,12 @@ public class PlayerController : MonoBehaviour
 
 	private void OnPress(InputAction.CallbackContext context)
     {
+		if (!photonView.IsMine) return;
+
 		screenPosition = playerActionMap["ScreenPosition"].ReadValue<Vector2>();
 		//Debug.Log($"Screen position: {screenPosition}");
 
-		Ray ray = mainCamera.ScreenPointToRay(screenPosition);
+		Ray ray = playerCamera.ScreenPointToRay(screenPosition);
 		RaycastHit hit;
 
 		if (Physics.Raycast(ray, out hit))
@@ -136,14 +179,18 @@ public class PlayerController : MonoBehaviour
 
 	private void OnPan(InputAction.CallbackContext	context)
 	{
+		if (!photonView.IsMine) return;
+
 		Vector2 delta = context.ReadValue<Vector2>();
 
 		Vector3 movement = new Vector3(delta.y, 0, -delta.x) * panSpeed * Time.deltaTime;  // Pan affects the x and z axis
-		mainCamera.transform.parent.transform.Translate(movement, Space.World);
+		playerCameraController.transform.Translate(movement, Space.World);
 	}
 
 	private void OnZoom(InputAction.CallbackContext context)
 	{
+		if (!photonView.IsMine) return;
+
 		float zoom = context.ReadValue<Vector2>().y;
 
 		// Set rotation of camera
@@ -154,7 +201,7 @@ public class PlayerController : MonoBehaviour
 
 		// Zoom happens along the calculated forward direction
 		Vector3 movement = zoom * zoomSpeed * Time.deltaTime * calculatedForward;
-		mainCamera.transform.parent.transform.Translate(movement, Space.Self);
+		playerCameraController.transform.Translate(movement, Space.Self);
 	}
 
 	public void SetPlayerCheckerColor(Checker.CheckerColor checkerColor)
