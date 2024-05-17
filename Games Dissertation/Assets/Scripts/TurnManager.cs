374f206ba460af840602ccf44bf5d2e9398e7b40
April Hussey <april.hussey@outlook.com>
2024-05-17 01:51:24 +0100

    Added basic UI to show which player's turn it is

--- Games Dissertation/Assets/Scripts/TurnManager.cs
+++ Games Dissertation/Assets/Scripts/TurnManager.cs
@@ -1,6 +1,7 @@
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TurnManager : MonoBehaviourPunCallbacks
@@ -8,9 +9,15 @@ public class TurnManager : MonoBehaviourPunCallbacks
 	// The player whose turn it is
 	public Checker.CheckerColor currentPlayer;
 
+	[SerializeField]
+	private GameObject whiteCheckersTurn;
+	[SerializeField]
+	private GameObject blackCheckersTurn;
+
 	void Start()
 	{
 		currentPlayer = Checker.CheckerColor.White;
+		UpdateCurrentPlayerCanvas(currentPlayer);
 	}
 
 	// Call this method to switch to the next player's turn
@@ -26,8 +33,23 @@ public class TurnManager : MonoBehaviourPunCallbacks
 	}
 
 	[PunRPC]
-	void SyncCurrentPlayer(int player)
+	public void SyncCurrentPlayer(int player)
 	{
 		currentPlayer = (Checker.CheckerColor)player;
+		UpdateCurrentPlayerCanvas(currentPlayer);
+	}
+
+	public void UpdateCurrentPlayerCanvas(Checker.CheckerColor currentPlayer)
+	{
+		if (currentPlayer == Checker.CheckerColor.White)
+		{
+			whiteCheckersTurn.SetActive(true);
+			blackCheckersTurn.SetActive(false);
+		}
+		if (currentPlayer == Checker.CheckerColor.Black)
+		{
+			blackCheckersTurn.SetActive(true);
+			whiteCheckersTurn.SetActive(false);
+		}
 	}
 }
